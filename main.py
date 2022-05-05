9de204f75948b620bca3867fab2ad138886f9c15
José Luis Romero <jose.romero@ing.uchile.cl>
2022-05-04 22:20:53 -0400

    Only notifies the user when the data has changed

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from scrape import fetch_formatted_data
 from bot import send_message
 
+CACHE_FILE = "cache.txt"
+
+
+def has_changed(str):
+    changed = False
+    with open(CACHE_FILE, "r+") as f:
+        prev = f.read()
+        if str != prev:
+            changed = True
+            f.seek(0)
+            f.write(str)
+            f.truncate()
+    return changed
+
 
 def main():
-    msg = fetch_formatted_data()
-    if msg:
-        send_message(msg)
+    formatted_data = fetch_formatted_data() or 'No hay cupos disponibles :('
+    truncated = "\n".join(formatted_data.split("\n")[:-1])
+    if has_changed(truncated):
+        send_message(formatted_data)
 
 
 if __name__ == "__main__":
