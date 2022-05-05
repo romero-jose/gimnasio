fcecf45dd47d28d944f85e23b66dddb904e1edd5
José Luis Romero <jose.romero@ing.uchile.cl>
2022-05-04 23:10:22 -0400

    Adds logging

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ def has_changed(str):
 
 def main():
     formatted_data = fetch_formatted_data() or "No hay cupos disponibles :("
+    print(formatted_data)
     truncated = "\n".join(formatted_data.split("\n")[:-1])
     if has_changed(truncated):
         send_message(formatted_data)
