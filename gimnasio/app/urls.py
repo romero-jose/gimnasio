954476b26a9262bfd0a48d67d1818711a8e2831d
José Luis Romero <jose.romero@ing.uchile.cl>
2022-10-02 18:55:49 -0300

    Fixes formatting

--- gimnasio/app/urls.py
+++ gimnasio/app/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("", include('reservations.urls')),
+    path("", include("reservations.urls")),
     path("admin/", admin.site.urls),
 ]
