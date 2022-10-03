6e404cc287f4052f3cfc2de5581dcabd3b0daca0
José Luis Romero <jose.romero@ing.uchile.cl>
2022-10-02 23:08:21 -0300

    Adds task that notifies users of available slots of their preference

--- gimnasio/lib/bot.py
+++ gimnasio/lib/bot.py
@@ -2,10 +2,9 @@ import os
 
 from telegram import Bot
 
-CHAT_ID = os.environ["CHAT_ID"]
 TOKEN = os.environ["TELEGRAM_TOKEN"]
 
 
-def send_message(message):
+def send_message(message, chat_id):
     bot = Bot(token=TOKEN)
-    bot.sendMessage(chat_id=CHAT_ID, text=message)
+    bot.sendMessage(chat_id, text=message)
--- gimnasio/reservations/tasks.py
+++ gimnasio/reservations/tasks.py
@@ -2,9 +2,11 @@ import datetime
 
 import reservations.models as models
 import lib.scrape as scrape
+import lib.bot as bot
 
+from django.db.models import Count, F
 from huey import crontab
-from huey.contrib.djhuey import db_periodic_task
+from huey.contrib.djhuey import db_task, db_periodic_task
 
 
 def update_slots(data):
@@ -31,3 +33,59 @@ def fetch_and_update_slots():
     data = scrape.fetch_data()
     update_slots(data)
     print("Updated slots")
+
+    notify_users_open_slots()
+
+
+@db_task
+def notify_users_open_slots():
+    # Get available slots
+    available_slots = (
+        models.Reservation.objects.select_related("slot")
+        .values("slot_id")
+        .annotate(count=Count("slot_id"))
+        .filter(count__lt=F("slot__capacity"))
+        .values_list(
+            "slot_id",
+            "count",
+            "slot__capacity",
+            "slot__block__start_time",
+            "slot__block_id",
+            "slot__date",
+        )
+    )
+    blocks = {}
+    for slot_id, count, capacity, start_time, block_id, date in available_slots:
+        if block_id not in blocks:
+            blocks[block_id] = {}
+        blocks[block_id][slot_id] = {
+            "count": count,
+            "capacity": capacity,
+            "start_time": start_time,
+            "date": date,
+        }
+
+    # Notify users
+    for user in models.User.objects.all():
+        block_ids = models.BlockPreference.objects.filter(user=user).values("id")
+        results = models.Slot.objects.filter(block_id__in=block_ids).values(
+            "id", "block_id"
+        )
+        result_list = list(results)
+        if len(result_list) == 0:
+            continue
+        else:
+            msg = []
+            for slot_id, block_id in result_list:
+                if block_id not in blocks:
+                    continue
+                d = blocks[block_id]
+                msg.append(
+                    f"{d['date']} {d['start_time']} {d['count']}/{d['capacity']} cupos"
+                )
+        if msg:
+            message = "\n".join("Hay cupos disponibles:" + msg)
+            bot.send_message(message, user.chat_id)
+        else:
+            pass
+    print("Notified users")
